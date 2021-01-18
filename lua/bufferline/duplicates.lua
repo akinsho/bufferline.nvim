5b8a249fa205d3081f9eb416cb79813296f0f180
Akin Sowemimo <akin.sowemimo@gmail.com>
2021-01-18 22:02:11 +0000

    feat: add short circuit to while loop
    in case we are unable to resolve duplicate dirs

--- lua/bufferline/duplicates.lua
+++ lua/bufferline/duplicates.lua
@@ -1,5 +1,5 @@
-require'bufferline/buffers'
-local constants = require'bufferline/constants'
+require "bufferline/buffers"
+local constants = require "bufferline/constants"
 
 local M = {}
 
@@ -28,9 +28,17 @@ local function mark_duplicates(buffers, current, callback)
     duplicates[current.filename] = {current}
   else
     local depth = 1
+    local limit = 10
     for _, buf in ipairs(duplicate) do
       local buf_depth = 1
       while current:ancestor(buf_depth) == buf:ancestor(buf_depth) do
+        -- short circuit if we have gone up 10 directories, we don't expect to have
+        -- to look that far to find a non-matching ancestor and we might be looping
+        -- endlessly
+        if buf_depth >= limit then
+          return
+        end
+
         buf_depth = buf_depth + 1
       end
       if buf_depth > depth then
@@ -75,15 +83,15 @@ end
 --- @param depth number
 --- @param max_size number
 local function truncate(dir, depth, max_size)
-    if #dir <= max_size then
-      return dir
-    end
-    local marker = "…"
-    -- we truncate any section of the ancestor which is too long
-    -- by dividing the alloted space for each section by the depth i.e.
-    -- the amount of ancestors which will be prefixed
-    local allowed_size = math.ceil(max_size / depth)
-    return dir:sub(0, allowed_size  - strwidth(marker)) .. marker
+  if #dir <= max_size then
+    return dir
+  end
+  local marker = "…"
+  -- we truncate any section of the ancestor which is too long
+  -- by dividing the alloted space for each section by the depth i.e.
+  -- the amount of ancestors which will be prefixed
+  local allowed_size = math.ceil(max_size / depth)
+  return dir:sub(0, allowed_size - strwidth(marker)) .. marker
 end
 
 --- @param context table
@@ -97,9 +105,13 @@ function M.component(context)
   -- user if we are going to potentially increase the tab length by
   -- prefixing it with the parent dir(s)
   if buffer.duplicated and not options.enforce_regular_tabs then
-    local dir = buffer:ancestor(buffer.prefix_count, function(dir, depth)
-      return truncate(dir, depth, options.max_prefix_length)
-    end)
+    local dir =
+      buffer:ancestor(
+      buffer.prefix_count,
+      function(dir, depth)
+        return truncate(dir, depth, options.max_prefix_length)
+      end
+    )
     component = padding .. hl.duplicate .. dir .. hl.background .. component
     length = length + strwidth(padding .. dir)
   else
