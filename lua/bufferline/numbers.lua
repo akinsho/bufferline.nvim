75309d01e19a358f1fafd52d61d81ad1db04cd0c
Jerry Wang <wangjianjun@gmail.com>
2021-04-01 20:48:40 +0800

    Changed mode to "both"
    by default, when number_style is "superscript", it will display buffer id at top
    left and ordinal number at bottom right together. the user from
    lightline-bufferline may get used to it.
    
    if number_style is "none", it will be 5(3) , not looking good.

--- lua/bufferline/numbers.lua
+++ lua/bufferline/numbers.lua
@@ -3,36 +3,33 @@ local constants = require "bufferline/constants"
 local M = {}
 
 local superscript_numbers = {
-  [0] = "⁰",
-  [1] = "¹",
-  [2] = "²",
-  [3] = "³",
-  [4] = "⁴",
-  [5] = "⁵",
-  [6] = "⁶",
-  [7] = "⁷",
-  [8] = "⁸",
-  [9] = "⁹",
-  [10] = "¹⁰",
-  [11] = "¹¹",
-  [12] = "¹²",
-  [13] = "¹³",
-  [14] = "¹⁴",
-  [15] = "¹⁵",
-  [16] = "¹⁶",
-  [17] = "¹⁷",
-  [18] = "¹⁸",
-  [19] = "¹⁹",
-  [20] = "²⁰"
+  ['0'] = "⁰",
+  ['1'] = "¹",
+  ['2'] = "²",
+  ['3'] = "³",
+  ['4'] = "⁴",
+  ['5'] = "⁵",
+  ['6'] = "⁶",
+  ['7'] = "⁷",
+  ['8'] = "⁸",
+  ['9'] = "⁹",
 }
 
--- styled numbers at top half
-local t_numbers = { ['0'] = "⁰", ['1'] = "¹", ['2'] = "²", ['3'] = "³", ['4'] = "⁴", ['5'] = "⁵", ['6'] = "⁶", ['7'] = "⁷", ['8'] = "⁸", ['9'] = "⁹" }
--- styled numbers at down half
-local d_numbers = { ['0'] = '₀', ['1'] = '₁', ['2'] = '₂', ['3'] = '₃', ['4'] = '₄', ['5'] = '₅', ['6'] = '₆', ['7'] = '₇', ['8'] = '₈', ['9'] = '₉' }
+local subscript_numbers = {
+  ['0'] = '₀',
+  ['1'] = '₁',
+  ['2'] = '₂',
+  ['3'] = '₃',
+  ['4'] = '₄',
+  ['5'] = '₅',
+  ['6'] = '₆',
+  ['7'] = '₇',
+  ['8'] = '₈',
+  ['9'] = '₉',
+}
 
 -- from number to the styled number
-local trans_number = function (t, n)
+local convert_to_styled_num = function (t, n)
   local n = tostring(n)
   local r = ""
   for i = 1, #n do
@@ -42,13 +39,17 @@ local trans_number = function (t, n)
 end
 
 local function prefix(buffer, mode, style)
-  -- if mode is mix, it will be like lightline-bufferline, buffer_id at top left
+  -- if mode is both, it will be like lightline-bufferline, buffer_id at top left
   -- and ordinal number at bottom down, so the user can get the buffer number
-  if mode == "mix" then
-    return trans_number(t_numbers, buffer.id) .. trans_number(d_numbers, buffer.ordinal)
+  if mode == "both" then
+    if style == "superscript" then
+      return convert_to_styled_num(superscript_numbers, buffer.id) .. convert_to_styled_num(subscript_numbers, buffer.ordinal)
+    else
+      return buffer.id .. "(" .. buffer.ordinal .. ")"
+    end
   else
     local n = mode == "ordinal" and buffer.ordinal or buffer.id
-    local num = style == "superscript" and superscript_numbers[n] or n .. "."
+    local num = style == "superscript" and convert_to_styled_num(superscript_numbers,n) or n .. "."
     return num
   end
 end
