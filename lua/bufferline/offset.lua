808b229f1ef08c64a0b5e79ba369e421eb37598c
lu5je0 <lu5je0@gmail.com>
2021-11-07 17:41:49 +0800

    return highlight .. text .. ""

--- lua/bufferline/offset.lua
+++ lua/bufferline/offset.lua
@@ -49,7 +49,7 @@ local function get_section_text(size, highlight, offset)
     end
     text = string.rep(" ", left) .. text .. string.rep(" ", right)
   end
-  return highlight .. text
+  return highlight .. text .. ""
 end
 
 ---A heuristic to attempt to derive a windows background color from a winhighlight
