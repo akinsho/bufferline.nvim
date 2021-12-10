19f19a552f771cc7135aa13b32e81f188639f451
Andrea Cappuccio <hood@null.net>
2021-12-10 19:12:58 +0100

    differentiate diagnostics retrieval between neovim versions above or equal 0.7 or below 0.6

--- lua/bufferline/diagnostics.lua
+++ lua/bufferline/diagnostics.lua
@@ -51,7 +51,8 @@ local function is_disabled(diagnostics)
   if
     not diagnostics
     or not vim.tbl_contains({ "nvim_lsp", "coc" }, diagnostics)
-    or (diagnostics == "nvim_lsp" and not vim.diagnostic.get)
+    or (diagnostics == "nvim_lsp" and vim.fn.has('nvim-0.7') and not vim.diagnostic.get) -- <-- Anything 0.7+
+    or (diagnostics == "nvim_lsp" and not vim.fn.has('nvim-0.7') and not vim.lsp.diagnostic.get) -- <- Anything below 0.7
     or (diagnostics == "coc" and vim.g.coc_service_initialized ~= 1)
   then
     return true
@@ -66,7 +67,11 @@ end
 
 local get_diagnostics = {
   nvim_lsp = function()
-    return vim.diagnostic.get()
+    if vim.fn.has('nvim-0.7') then
+      return vim.diagnostic.get()
+    else
+      return vim.lsp.diagnostic.get()
+    end
   end,
 
   coc = (function()
