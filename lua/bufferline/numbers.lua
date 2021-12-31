e12c3241a3828e5c63d17dbbc6370c1aa0efb922
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2021-12-31 17:12:11 +0300

    chore: no extra padding if numbers func returns empty string

--- lua/bufferline/numbers.lua
+++ lua/bufferline/numbers.lua
@@ -121,7 +121,10 @@ function M.component(context)
     return context
   end
   local number_prefix = prefix(buffer, options.numbers, options.number_style)
-  local number_component = number_prefix .. constants.padding
+  local number_component = ""
+  if number_prefix ~= "" then
+    number_component = number_prefix .. constants.padding
+  end
   component = number_component .. component
   length = length + vim.fn.strwidth(number_component)
   return context:update({ component = component, length = length })
