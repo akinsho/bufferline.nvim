e08ba9b60dba7cc4448427b3e38e37dedf13b886
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-18 22:05:10 +0000

    refactor(set): store ordered list of values

--- lua/bufferline/set.lua
+++ lua/bufferline/set.lua
@@ -1,14 +1,15 @@
 ---@class Set
 ---@field private values table<string, number>
+---@field private list number[]
 local Set = {}
 
 ---@param list integer[]
 ---@return Set
 function Set:new(list)
   list = list or {}
-  local set = { values = {} }
-  for index, l in ipairs(list) do
-    set.values[tostring(l)] = index
+  local set = { values = {}, list = list }
+  for _, l in ipairs(list) do
+    set.values[tostring(l)] = true
   end
   setmetatable(set, self)
   self.__index = self
@@ -18,8 +19,9 @@ end
 ---@param item number
 ---@param index number
 function Set:add(item, index)
-  if not self.values[item] then
-    self.values[tostring(item)] = index
+  if not self.values[tostring(item)] then
+    self.values[tostring(item)] = true
+    table.insert(self.list, index or #self.list + 1, item)
   end
 end
 
@@ -29,26 +31,30 @@ function Set:has(item)
   return self.values[tostring(item)] ~= nil
 end
 
+function Set:size()
+  return #self.list
+end
+
 ---@param item number
 function Set:remove(item)
   if self.values[item] then
     self.values[item] = nil
+    self.list = vim.tbl_filter(function(i)
+      return i ~= item
+    end, self.list)
   end
 end
 
 ---@return number[]
 function Set:get_all()
-  local result = {}
-  for key, value in pairs(self.values) do
-    table.insert(result, value, tonumber(key))
-  end
-  return result
+  return self.list
 end
 
 ---@param list number[]
-function Set:add_all(list)
-  for index, l in ipairs(list) do
-    self:add(l, #self.values + index)
+---@param insertion_index number?
+function Set:add_all(list, insertion_index)
+  for _, item in ipairs(list) do
+    self:add(item, insertion_index)
   end
 end
 
@@ -57,13 +63,19 @@ end
 function Set:intersection(list)
   local result = {}
   local set_b = Set:new(list)
-  local items = self:get_all()
-  for _, item in ipairs(items) do
+  for _, item in ipairs(self.list) do
     if set_b:has(item) then
       table.insert(result, item)
     end
   end
-  return result
+  return result, set_b
+end
+
+function Set:replace_with_intersection(list)
+  local intersect, set_b = self:intersection(list)
+  self.list = intersect
+  self.values = set_b.values
+  return self.list
 end
 
 return Set
