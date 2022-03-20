bd634e4cb3b131051f88722d420f601b3e0abfbb
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-20 19:24:32 +0000

    refactor(sorters): return early if sort_by is none

--- lua/bufferline/sorters.lua
+++ lua/bufferline/sorters.lua
@@ -94,11 +94,12 @@ end
 --- @param elements TabElement[]
 function M.sort(elements)
   local sort_by = config.options.sort_by
-  local is_tabline = config:is_tabline()
-
+  -- DO NOTHING
   if sort_by == "none" then
-    -- DO NOTHING, FIXME: can/should we completely avoid this function call
-  elseif sort_by == "extension" then
+    return
+  end
+
+  if sort_by == "extension" then
     table.sort(elements, sort_by_extension)
   elseif sort_by == "directory" then
     table.sort(elements, sort_by_directory)
@@ -107,6 +108,7 @@ function M.sort(elements)
   elseif sort_by == "id" then
     table.sort(elements, sort_by_id)
   elseif sort_by == "tabs" then
+    local is_tabline = config:is_tabline()
     table.sort(elements, is_tabline and sort_by_id or sort_by_tabs)
   elseif type(sort_by) == "function" then
     table.sort(elements, sort_by)
