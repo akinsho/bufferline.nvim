4ef00f2b9f354f0f2c77b023d3291828f175e1ec
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-04-02 23:52:04 +0100

    fix(ui): render duplicate prefix before groups

--- lua/bufferline/ui.lua
+++ lua/bufferline/ui.lua
@@ -494,12 +494,12 @@ function M.element(state, buffer)
   --- length of the component
   ctx = utils.compose(
     get_name,
+    add_duplicates,
+    add_group,
     add_padding({ right = 1 }),
     add_diagnostics,
-    add_duplicates,
     add_prefix,
     add_numbers,
-    add_group,
     add_spacing,
     add_click_action,
     add_indicator,
