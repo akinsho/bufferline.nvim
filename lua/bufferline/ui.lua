564857e4ec2d8600360fbbc812975779692cd164
Akin Sowemimo <akin@email.com>
2022-05-08 20:15:59 +0200

    refactor(ui): lazy import groups module

--- lua/bufferline/ui.lua
+++ lua/bufferline/ui.lua
@@ -14,6 +14,8 @@ local highlights = lazy.require("bufferline.highlights")
 local colors = require("bufferline.colors")
 ---@module "bufferline.pick"
 local pick = lazy.require("bufferline.pick")
+---@module "bufferline.groups"
+local groups = lazy.require("bufferline.groups")
 
 local M = {}
 local visibility = constants.visibility
@@ -484,8 +486,6 @@ function M.element(state, element)
   local add_diagnostics = require("bufferline.diagnostics").component
   local add_duplicates = require("bufferline.duplicates").component
   local add_numbers = require("bufferline.numbers").component
-  local add_group = buffer.group and require("bufferline.groups").component or utils.identity
-
   --- Order matter here as this is the sequence which builds up the tab component
   --- each render function takes the context and returns an updated context with it's
   --- changes e.g. adding a modified icon to the context component or updating the
@@ -508,7 +508,7 @@ function M.element(state, element)
 
   local name = get_name(ctx)
   local duplicate_prefix = add_duplicates(ctx)
-  local group_prefix = add_group(ctx)
+  local group_item = element.group and groups.component(ctx) or nil
   local diagnostic = add_diagnostics(ctx)
   local icon = add_icon(ctx)
   local number_prefix = add_numbers(ctx)
