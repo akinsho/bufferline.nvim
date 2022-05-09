1c8e845389cd109621c2214a2afff75007457509
Gabriele Musco <emaildigabry@gmail.com>
2022-05-09 08:06:44 +0200

    feat(tabpages): add custom_filter (#400)
    Co-authored-by: Gabriele Musco <gabmus@disroot.org>

--- lua/bufferline/tabpages.lua
+++ lua/bufferline/tabpages.lua
@@ -52,21 +52,42 @@ function M.get()
   return tabpages
 end
 
----Choose the active window's buffer as the buffer for that tab
 ---@param tab_num integer
----@return string
----@return number
-local function get_tab_buffer_details(tab_num)
+---@return integer
+local function get_active_buf_for_tab(tab_num)
   local window = api.nvim_tabpage_get_win(tab_num)
-  local buf = api.nvim_win_get_buf(window)
+  return api.nvim_win_get_buf(window)
+end
+
+---Choose the active window's buffer as the buffer for that tab
+---@param buf integer
+---@return string
+local function get_buffer_name(buf)
   local name = (buf and api.nvim_buf_is_valid(buf)) and api.nvim_buf_get_name(buf) or "[No name]"
-  return name, buf
+  return name
 end
 
 local function get_valid_tabs()
   return vim.tbl_filter(function(t)
     return api.nvim_tabpage_is_valid(t)
   end, api.nvim_list_tabpages())
+end
+
+---Filter the buffers to show based on the user callback passed in
+---@param buf_nums integer[]
+---@param callback fun(buf: integer, bufs: integer[]): boolean
+---@return integer[]
+local function apply_buffer_filter(buf_nums, callback)
+  if type(callback) ~= "function" then
+    return buf_nums
+  end
+  local filtered = {}
+  for _, buf in ipairs(buf_nums) do
+    if callback(buf, buf_nums) then
+      table.insert(filtered, buf)
+    end
+  end
+  return next(filtered) and filtered or buf_nums
 end
 
 --- Get tab buffers based on open windows within the tab
@@ -90,9 +111,19 @@ function M.get_components(state)
   local components = {}
   pick.reset()
 
+  local filter = options.custom_filter
+
   for i, tab_num in ipairs(tabs) do
+    local active_buf = get_active_buf_for_tab(tab_num)
     local buffers = get_tab_buffers(tab_num)
-    local path, buf_num = get_tab_buffer_details(tab_num)
+    local buffer
+    if filter then
+      buffers = apply_buffer_filter(buffers, filter)
+      buffer = filter(active_buf) and active_buf or buffers[1]
+    else
+      buffer = active_buf
+    end
+    local path = get_buffer_name(buffer)
     local all_diagnostics = diagnostics.get(options)
     -- TODO: decide how diagnostics should render if the focused
     -- window doesn't have any errors but a neighbouring window does
@@ -101,11 +132,11 @@ function M.get_components(state)
     -- end)
     local tab = Tabpage:new({
       path = path,
-      buf = buf_num,
+      buf = buffer,
       buffers = buffers,
       id = tab_num,
       ordinal = i,
-      diagnostics = all_diagnostics[buf_num],
+      diagnostics = all_diagnostics[buffer],
       name_formatter = options.name_formatter,
       hidden = false,
       focusable = true,
