9f3deb741214a70a64230b53e4d9f6aa41bb6b65
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2022-08-05 15:24:54 +0100

    refactor(highlights): make switch more concise
    rather than a repetitive and long switch statement improve the selection
    of styles per state with a helper function

--- lua/bufferline/highlights.lua
+++ lua/bufferline/highlights.lua
@@ -156,70 +156,46 @@ local function add_element_group_hl(element, hls, current_hl)
     utils.log.debug(fmt("%s group highlight not found", name))
   end
 end
+
+--- Get the current style for a highlight depending on visibility state
+---@param vis 1|2|3
+---@param hls BufferlineHighlights
+---@return fun(name: string, base: string?): BufferlineHLGroup
+local function style_per_state(vis, hls)
+  local v = constants.visibility
+  return function(name, base)
+    if not base then base = name end
+    local s = { [v.INACTIVE] = "%s_visible", [v.SELECTED] = "%s_selected" }
+    return hls[s[vis] and fmt(s[vis], name) or base] or {}
+  end
+end
+
 ---@param element Buffer | Tabpage
 ---@return table
 function M.for_element(element)
   local hl = {}
   local h = config.highlights
   if not h then return hl end
-  --- TODO: find a tidier way to do this if possible
-  if element:current() then
-    hl.background = h.buffer_selected.hl
-    hl.modified = h.modified_selected.hl
-    hl.duplicate = h.duplicate_selected.hl
-    hl.pick = h.pick_selected.hl
-    hl.separator = h.separator_selected.hl
-    hl.buffer = h.buffer_selected
-    hl.diagnostic = h.diagnostic_selected.hl
-    hl.error = h.error_selected.hl
-    hl.error_diagnostic = h.error_diagnostic_selected.hl
-    hl.warning = h.warning_selected.hl
-    hl.warning_diagnostic = h.warning_diagnostic_selected.hl
-    hl.info = h.info_selected.hl
-    hl.info_diagnostic = h.info_diagnostic_selected.hl
-    hl.hint = h.hint_selected.hl
-    hl.hint_diagnostic = h.hint_diagnostic_selected.hl
-    hl.close_button = h.close_button_selected.hl
-    hl.numbers = h.numbers_selected.hl
-  elseif element:visible() then
-    hl.background = h.buffer_visible.hl
-    hl.modified = h.modified_visible.hl
-    hl.duplicate = h.duplicate_visible.hl
-    hl.pick = h.pick_visible.hl
-    hl.separator = h.separator_visible.hl
-    hl.buffer = h.buffer_visible
-    hl.diagnostic = h.diagnostic_visible.hl
-    hl.error = h.error_visible.hl
-    hl.error_diagnostic = h.error_diagnostic_visible.hl
-    hl.warning = h.warning_visible.hl
-    hl.warning_diagnostic = h.warning_diagnostic_visible.hl
-    hl.info = h.info_visible.hl
-    hl.info_diagnostic = h.info_diagnostic_visible.hl
-    hl.hint = h.hint_visible.hl
-    hl.hint_diagnostic = h.hint_diagnostic_visible.hl
-    hl.close_button = h.close_button_visible.hl
-    hl.numbers = h.numbers_visible.hl
-  else
-    hl.background = h.background.hl
-    hl.modified = h.modified.hl
-    hl.duplicate = h.duplicate.hl
-    hl.pick = h.pick.hl
-    hl.separator = h.separator.hl
-    hl.buffer = h.background
-    hl.diagnostic = h.diagnostic.hl
-    hl.error = h.error.hl
-    hl.error_diagnostic = h.error_diagnostic.hl
-    hl.warning = h.warning.hl
-    hl.warning_diagnostic = h.warning_diagnostic.hl
-    hl.info = h.info.hl
-    hl.info_diagnostic = h.info_diagnostic.hl
-    hl.hint = h.hint.hl
-    hl.hint_diagnostic = h.hint_diagnostic.hl
-    hl.close_button = h.close_button.hl
-    hl.numbers = h.numbers.hl
-  end
 
-  if element.group then groups.set_current_hl(element, h, hl) end
+  local current_state = style_per_state(element:visibility(), h)
+
+  hl.modified = current_state("modified").hl
+  hl.duplicate = current_state("duplicate").hl
+  hl.pick = current_state("pick").hl
+  hl.separator = current_state("separator").hl
+  hl.diagnostic = current_state("diagnostic").hl
+  hl.error = current_state("error").hl
+  hl.error_diagnostic = current_state("error_diagnostic").hl
+  hl.warning = current_state("warning").hl
+  hl.warning_diagnostic = current_state("warning_diagnostic").hl
+  hl.info = current_state("info").hl
+  hl.info_diagnostic = current_state("info_diagnostic").hl
+  hl.hint = current_state("hint").hl
+  hl.hint_diagnostic = current_state("hint_diagnostic").hl
+  hl.close_button = current_state("close_button").hl
+  hl.numbers = current_state("numbers").hl
+  hl.buffer = current_state("buffer", "background")
+  hl.background = hl.buffer.hl
 
   add_element_group_hl(element, h, hl)
   return hl
