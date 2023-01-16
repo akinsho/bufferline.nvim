8d5f72c2d95c79d9ca9cefd68b981e4d0a1a4e38
Taaqif Peck (Tye) <taaqifpeck@gmail.com>
2023-01-16 13:24:36 +0800

    fix: updated slope

--- lua/bufferline/ui.lua
+++ lua/bufferline/ui.lua
@@ -271,7 +271,7 @@ end
 ---@param style string
 ---@return boolean
 local function is_slant(style)
-  return vim.tbl_contains({ sep_names.slant, sep_names.padded_slant,  sep_names.padded_slope}, style)
+  return vim.tbl_contains({ sep_names.slant, sep_names.padded_slant, sep_names.slope, sep_names.padded_slope}, style)
 end
 
 --- "▍" "░"
