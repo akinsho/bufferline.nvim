320ef7e10a5df82e8e9dbac22e42d9b6c6d25a7d
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-02-16 01:10:01 +0000

    fix error when empty

--- lua/bufferline/groups.lua
+++ lua/bufferline/groups.lua
@@ -301,6 +301,11 @@ function M.command(group_name, callback)
     function(list) return list.name == group_name end,
     state.components_by_group
   )
+
+  if not group then
+    return
+  end
+
   utils.for_each(callback, group)
 end
 
