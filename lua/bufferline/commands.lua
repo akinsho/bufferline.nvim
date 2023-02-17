7a0d6b52ab4a5df5ef5a26bec6c206f468f61bab
FISH UP <68397336+MisterFISHUP@users.noreply.github.com>
2023-02-17 13:02:28 +0000

    docs: add comment in `move_to`

--- lua/bufferline/commands.lua
+++ lua/bufferline/commands.lua
@@ -149,11 +149,13 @@ function M.get_current_element_index(current_state, opts)
   end
 end
 
---- @param to_index number
+--- Move the buffer at index `from_index` (or current index if not specified) to position `to_index`
+--- @param to_index number negative indices are accepted (counting from the right instead of the left, e.g. -1 for the last position, -2 for the second-last, etc.)
 --- @param from_index number?
 function M.move_to(to_index, from_index)
   local index = from_index or M.get_current_element_index(state)
   if not index then return utils.notify("Unable to find buffer to move, sorry", "warn") end
+  -- Calculate next index depending on the sign of `to_index`
   local next_index = to_index > 0 and to_index or #state.components + 1 + to_index
   if next_index >= 1 and next_index <= #state.components then
     local item = state.components[index]
