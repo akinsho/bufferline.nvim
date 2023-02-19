8b2673ae7525b81e58b6bc21b49755f6a60b595a
Taaqif Peck (Tye) <taaqifpeck@gmail.com>
2023-02-19 11:21:32 +0800

    fix: re-add padded_slant keys

--- lua/bufferline/constants.lua
+++ lua/bufferline/constants.lua
@@ -11,6 +11,7 @@ M.sep_names = {
   thick = "thick",
   slant = "slant",
   slope = "slope",
+  padded_slant = "padded_slant",
 }
 
 ---@type table<string, string[]>
@@ -19,6 +20,7 @@ M.sep_chars = {
   [M.sep_names.thick] = { "▌", "▐" },
   [M.sep_names.slant] = { "", "" },
   [M.sep_names.slope] = { "", "" },
+  [M.sep_names.padded_slant] = { "" .. M.padding, "" .. M.padding },
 }
 
 M.positions_key = "BufferlinePositions"
