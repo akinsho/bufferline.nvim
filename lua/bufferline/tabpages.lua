0f3d59e5936f7340b736a13a9f28c810dafc6644
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2023-03-28 00:20:40 +0200

    refactor

--- lua/bufferline/tabpages.lua
+++ lua/bufferline/tabpages.lua
@@ -30,18 +30,16 @@ local function render(tabpage, is_active, style, highlights)
   local separator_hl = is_active and h.tab_separator_selected.hl_group or h.tab_separator.hl_group
   local chars = constants.sep_chars[style] or constants.sep_chars.thin
   local name = padding .. tabpage.tabnr .. padding
-  local swapped_chars = require("bufferline.config").options.separator_style == "thick"
-    or require("bufferline.config").options.separator_style == "thin"
+  local char_order = ({ thick = { 1, 2 }, thin = { 1, 2 } })[style] or { 2, 1 }
   return {
-    { highlight = separator_hl, text = chars[swapped_chars and 1 or 2] },
+    { highlight = separator_hl, text = chars[char_order[1]] },
     {
       highlight = hl,
       text = name,
       attr = { prefix = tab_click_component(tabpage.tabnr) },
     },
-    { highlight = separator_hl, text = chars[swapped_chars and 2 or 1] },
+    { highlight = separator_hl, text = chars[char_order[2]] },
   }
-
 end
 
 function M.get()
