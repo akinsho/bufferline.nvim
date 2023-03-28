fafc5d28a9a2b36cf8baf10150c05b803e0a8108
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2023-03-28 09:30:12 +0200

    feat(tabs): use separators matching current style (#714)

--- lua/bufferline/tabpages.lua
+++ lua/bufferline/tabpages.lua
@@ -29,11 +29,16 @@ local function render(tabpage, is_active, style, highlights)
   local hl = is_active and h.tab_selected.hl_group or h.tab.hl_group
   local separator_hl = is_active and h.tab_separator_selected.hl_group or h.tab_separator.hl_group
   local chars = constants.sep_chars[style] or constants.sep_chars.thin
-  local separator_component = chars[2]
-  local name = padding .. padding .. tabpage.tabnr .. padding
+  local name = padding .. tabpage.tabnr .. padding
+  local char_order = ({ thick = { 1, 2 }, thin = { 1, 2 } })[style] or { 2, 1 }
   return {
-    { highlight = hl, text = name, attr = { prefix = tab_click_component(tabpage.tabnr) } },
-    { highlight = separator_hl, text = separator_component },
+    { highlight = separator_hl, text = chars[char_order[1]] },
+    {
+      highlight = hl,
+      text = name,
+      attr = { prefix = tab_click_component(tabpage.tabnr) },
+    },
+    { highlight = separator_hl, text = chars[char_order[2]] },
   }
 end
 
