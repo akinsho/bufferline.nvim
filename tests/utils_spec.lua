7cde1a16f94e67efd2a262ce04f13a363fa24eb3
AThePeanut4 <49614525+AThePeanut4@users.noreply.github.com>
2023-06-27 18:44:18 +0200

    test: add save/restore tests

--- tests/utils_spec.lua
+++ tests/utils_spec.lua
@@ -18,4 +18,51 @@ describe("Utils tests", function()
     local truncated = utils.truncate_name("filename.md.md", 13)
     assert.is_equal(truncated, "filename.md" .. constants.ELLIPSIS)
   end)
+
+  it("should save/restore positions correctly", function()
+    -- remove existing buffers
+    vim.cmd("silent %bwipeout!")
+
+    local names = { "c.txt", "a.txt", "d.txt", "e.txt", "b.txt" }
+    local bufs = {}
+    for _, name in ipairs(names) do
+      vim.cmd.edit(name)
+      bufs[name] = api.nvim_get_current_buf()
+    end
+
+    local ids = {
+      bufs["a.txt"],
+      bufs["b.txt"],
+      bufs["c.txt"],
+      bufs["d.txt"],
+      bufs["e.txt"],
+    }
+
+    utils.save_positions(ids)
+
+    assert.same(utils.restore_positions(), ids)
+
+    -- restore_positions should not return invalid bufids
+
+    vim.cmd("bwipeout! " .. bufs["c.txt"])
+
+    ids = {
+      bufs["a.txt"],
+      bufs["b.txt"],
+      bufs["d.txt"],
+      bufs["e.txt"],
+    }
+    assert.same(utils.restore_positions(), ids)
+
+    vim.g[constants.positions_key] = '["INVALID_PATH"]'
+    assert.same(utils.restore_positions(), {})
+
+    -- empty or invalid JSON should return nil
+
+    vim.g[constants.positions_key] = "[]"
+    assert.is_equal(utils.restore_positions(), nil)
+
+    vim.g[constants.positions_key] = ""
+    assert.is_equal(utils.restore_positions(), nil)
+  end)
 end)
