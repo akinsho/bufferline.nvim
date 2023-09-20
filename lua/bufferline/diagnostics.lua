8a51c4b5d105d93fd2bc435bf93d4d5556fb2a60
Jamie Macdonald <jamie.alban@gmail.com>
2023-09-20 03:20:42 -0700

    fix(diagnostics): ignore disabled diagnostics (#816)
    * Ignore disabled diagnostics
    
    * Add nil check to vim.diagnostic.is_disabled

--- lua/bufferline/diagnostics.lua
+++ lua/bufferline/diagnostics.lua
@@ -71,8 +71,11 @@ local get_diagnostics = {
     local results = {}
     local diagnostics = vim.diagnostic.get()
     for _, d in pairs(diagnostics) do
-      if not results[d.bufnr] then results[d.bufnr] = {} end
-      table.insert(results[d.bufnr], d)
+      -- TODO remove is_disabled nil check when 0.9 is stable
+      if vim.diagnostic.is_disabled == nil or not vim.diagnostic.is_disabled(d.bufnr, d.namespace) then
+        if not results[d.bufnr] then results[d.bufnr] = {} end
+        table.insert(results[d.bufnr], d)
+      end
     end
     return results
   end,
